baefbc1df3f4345eb2357c3b5bf640564623df92
dan3si <dennis.priadka@gmail.com>
2021-08-13 17:22:08 +0300

    fix

--- frontend/src/components/About/About.js
+++ frontend/src/components/About/About.js
@@ -9,7 +9,7 @@ function About() {
           <li className={styles.item}>Капсулы Juicy click устраняют неприятный запах и добавляют приятный вкус вашим стикам!</li>
           <li className={styles.item}>Капсулы полностью безопасны, подходят для пользователей IQOS, lil и обычных сигарет</li>
           <li className={styles.item}>Основа сделана из желатина, все ароматизаторы пищевые</li>
-          <li className={styles.item}>В упаковке 100 капсул</li>
+          <li className={styles.item}>В упаковке 100 штук</li>
           <li className={styles.item}>Лимитированная серия вкусов</li>
         </ul>
       </div>
--- frontend/src/components/Using/Using.js
+++ frontend/src/components/Using/Using.js
@@ -1,12 +1,13 @@
 import styles from './Using.module.scss'
 import usingVideoSRC from '../../video/using.mp4'
+import usingPoster from '../../img/decorations/using_poster.png'
 
 function Using() {
   return (
     <div className={styles.using} id="using">
       <h2 className={styles.heading}>Использование</h2>
 
-      <video className={styles.usingVideo} controls="controls">
+      <video className={styles.usingVideo} controls="controls" poster={usingPoster}>
         <source src={usingVideoSRC} />
       </video>
     </div>
