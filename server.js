b9e42968cd0ebddfbd42866799be80b619b99ab0
dan3si <dennis.priadka@gmail.com>
2021-08-13 17:39:56 +0300

    canged server port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const mailer = require('./mailer')
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 80
 
 const app = express()
 
